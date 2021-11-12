a0d727d5ac6abf9fd464bbd43f9c72551184b039
Neha Kumari <64440607+NehaK745@users.noreply.github.com>
2021-11-12 21:12:21 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import 'firebase/database';
 import 'firebase/storage';
 import 'firebase/firestore';
 const firebaseConfig = {
-    apiKey: "AIzaSyCwaUeYSWGxbN2_tbsKFDDVb1kjiSb97O8",
-    authDomain: "url-shortner-b9e51.firebaseapp.com",
-    projectId: "url-shortner-b9e51",
-    storageBucket: "url-shortner-b9e51.appspot.com",
-    messagingSenderId: "1061908295159",
-    appId: "1:1061908295159:web:3d5b7bfc6e815d9863d2f2"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
 const app = firebase.initializeApp(firebaseConfig);
 export const db = app.firestore();
