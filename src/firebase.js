09cb279ff7de69f5144ddc4e50d9417da0a30020
Neha Kumari <64440607+NehaK745@users.noreply.github.com>
2021-11-12 21:27:35 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,7 @@ import 'firebase/auth';
 import 'firebase/database';
 import 'firebase/storage';
 import 'firebase/firestore';
-// Put your Configuration here:
+// Paste your Configuration here:
 const firebaseConfig = {
     apiKey: "",
     authDomain: "",
